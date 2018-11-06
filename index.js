48d50a53ffb26e71214f017993313635e235459d
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-11-06 08:47:28 -0500

    update

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const zlib = require('zlib')
+const express = require('express')
+const spdy = require('spdy')
+const cors = require('cors')
+const morgan = require('morgan')
+const MBTiles = require('@mapbox/mbtiles')
+const config = require('config')
+
+const app = express()
+app.use(cors())
+app.use(morgan(config.get('morgan-format')))
+app.use(express.static(config.get('htdocs')))
+
